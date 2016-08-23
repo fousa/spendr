ee855e7227e12949a6715c8c3174baab30a6d5c4
Jelle Vandebeeck <jelle@fousa.be>
2016-08-23 13:27:27 +0200

    Setup empty application.

---
+++ Spendr/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Spendr
+//
+//  Created by Jelle Vandebeeck on 23/08/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
