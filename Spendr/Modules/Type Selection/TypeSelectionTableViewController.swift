30044e794305492c67fb8893e09b84b29e0f6f20
Jelle Vandebeeck <jelle@fousa.be>
2016-08-25 15:17:21 +0200

    Add a simple type selection controller before the input.

---
+++ Spendr/Modules/Type Selection/TypeSelectionTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSelectionTableViewController.swift
+//  Spendr
+//
+//  Created by Jelle Vandebeeck on 25/08/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import UIKit
+
+class TypeSelectionTableViewController: UITableViewController {
+    
+    // MARK: - View model
+    
+    private let viewModel = TypeSelectionTableViewModel()
+    
+}
---
+++ Spendr/Modules/Type Selection/TypeSelectionTableViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TypeSelectionTableViewModel.swift
+//  Spendr
+//
+//  Created by Jelle Vandebeeck on 25/08/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import Foundation
+
+class TypeSelectionTableViewModel {
+}
