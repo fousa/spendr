2ec23140570cabff51cc19d9ea208ab20eb4697d
Jelle Vandebeeck <jelle@fousa.be>
2016-08-27 13:20:31 +0200

    Add the ReactiveKit pods.

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'Spendr' do
+  use_frameworks!
+
+  pod 'ReactiveKit',   '~> 2.1'
+  pod 'ReactiveUIKit', '~> 2.0'
+end
---
+++ Pods/Target Support Files/Pods-Spendr/Pods-Spendr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spendr : NSObject
+@end
+@implementation PodsDummy_Pods_Spendr
+@end
