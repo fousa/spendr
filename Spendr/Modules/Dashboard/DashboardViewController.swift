5d463539a5176f03450d7ed0fc86b80de56aad54
Jelle Vandebeeck <jelle@fousa.be>
2016-09-01 16:37:01 +0200

    Add an empty dashboard storyboard.

---
+++ Spendr/Modules/Dashboard/DashboardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.swift
+//  Spendr
+//
+//  Created by Jelle Vandebeeck on 01/09/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+}
