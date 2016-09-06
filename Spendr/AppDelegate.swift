41d7c8c1269f74ee072d2b95dbd924f610880b26
Jelle Vandebeeck <jelle@fousa.be>
2016-09-06 14:15:08 +0200

    Add 3D actions from the home screen icon.

--- Spendr/AppDelegate.swift
+++ Spendr/AppDelegate.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
+enum ShortcutIdentifier: String {
+    case AddExpense = "add"
+    
+    init?(identifier: String) {
+        guard let shortIdentifier = identifier.componentsSeparatedByString(".").last else {
+            return nil
+        }
+        self.init(rawValue: shortIdentifier)
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsShortcutItemKey] as? UIApplicationShortcutItem {
+            handle(shortcut: shortcutItem)
+            return false
+        }
+        
         return true
     }
 
@@ -21,4 +37,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         CloudHandler.shared.fetchExpenseTypes()
         CloudHandler.shared.fetchExpenses(forMonth: NSDate())
     }
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        completionHandler(handle(shortcut: shortcutItem))
+    }
+    
+    // MARK: - Shortcuts
+    
+    private func handle(shortcut item: UIApplicationShortcutItem) -> Bool {
+        guard let identifier = ShortcutIdentifier(identifier: item.type) where identifier == .AddExpense else {
+            return false
+        }
+        
+        guard let controller = window?.rootViewController as? DashboardViewController else {
+            return false
+        }
+        
+        controller.dismissViewControllerAnimated(false, completion: nil)
+        controller.performSegueWithIdentifier("Add", sender: nil)
+        
+        return true
+    }
+    
 }
