e80b2311326bc77f2bbf25ae7d22a2cd6d038878
Jelle Vandebeeck <jelle@fousa.be>
2016-09-07 23:42:53 +0200

    Add category model.

---
+++ Spendr/Models/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  Spendr
+//
+//  Created by Jelle Vandebeeck on 07/09/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+import CoreData
+
+class Category: NSManagedObject {
+}
+
+extension Category {
+
+    @NSManaged var name: String
+    
+}
